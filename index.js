9daee42cece7f7105cb54865ee5f13bed0224308
gease <gease@mail.ru>
2022-07-25 23:57:28 +0200

    Fixed cors errors.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const router = require('./router')
 
@@ -13,6 +14,7 @@ mongoose.connect('mongodb://localhost:27017/local');
 const app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json({type: '*/*'}));
+app.use(cors());
 
 router(app);
 
